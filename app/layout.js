d23d30438b063deaf54f56de2d17d6963b6d78bc
sagar sangwan <iamsagarsangwan@gmail.com>
2024-07-16 01:05:05 +0530

    maybe this will work

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 import heroimage from "../public/heroimage.svg"
 import Metrics from "./metrics";
 import Head from 'next/head';
+import Script from 'next/script';
 
 
 export const metadata = {
@@ -65,13 +66,21 @@ export default function RootLayout({ children }) {
         <meta name="twitter:description" content={metadata.twitter.description} />
         <meta name="twitter:image" content={metadata.twitter.images[0].url} />
         {/* Microsoft Clarity Script */}
-        <Metrics />
       </Head>
       <body className={inter.className}>
         <Providers>
           <Navbar />
           <main className="container">
             {children}
+            <Script id="clarity-script" strategy="afterInteractive">
+              {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+          `}
+            </Script>
           </main>
         </Providers>
         <Metrics />
